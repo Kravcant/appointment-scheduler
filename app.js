20beec5bda8f89f5e959aa08d2ec06e841915c34
anthonykravchishin <tonykravc53@gmail.com>
2025-10-21 12:05:10 -0700

    Accepted merge

--- app.js
+++ app.js
@@ -6,8 +6,31 @@ const PORT = 3004;
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended:true }));
+
+const forms = [];
+
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+app.get('/admin', (req, res) => {
+    res.send(forms);
+    // res.sendFile(`${import.meta.dirname}/views/admin.html`);
+});
+
+
+app.post('/submit-form', (req, res) => {
+    const form = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        date: req.body.date,
+        time: req.body.time
+    }
+    forms.push(form);
+    console.log(forms);
+
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
 });
 
 app.listen(PORT, () => {
